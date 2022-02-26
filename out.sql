65da1783d80422bddf1330b4cd139e33735eff3d
fbaguem2021 <fbaguem2021@cepnet.net>
2022-02-26 13:14:51 +0000

    subida out.sql

---
+++ out.sql
@@ -0,0 +1,14 @@
+Changed database context to 'seguretat'.
+id          nom                                           actiu
+----------- --------------------------------------------- -----
+          1 Administrador                                     1
+          2 Professor                                         1
+          3 Alumne                                            1
+
+(3 rows affected)
+id          correu                                        nom                                           cognoms                                       contrasenya                                                                                                                                                                                                                                                      actiu rols_id    
+----------- --------------------------------------------- --------------------------------------------- --------------------------------------------- ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- ----- -----------
+          7 ffernandez@politecnics.barcelona              Francisco                                     Fernández Fernández                           $2a$11$Y7MFjX/wdZCMGEoBewbPwOKBJBsxNkvqgFQGMUurY2aHlZ./kzfES                                                                                                                                                                                                         1           1
+          8 ivarela@salesians.sarria.cat                  Isabel                                        Varela                                        $2a$11$6DOm0MPEfA2eqkylRITuDuds/9Y7ZOkj6GtzC2j5znlk5qAysd.1S                                                                                                                                                                                                         1           1
+
+(2 rows affected)
