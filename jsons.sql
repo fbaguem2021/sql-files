1d8d3777a7a9327a4331c078664ca409df05e820
fbaguem2021 <fbaguem2021@cepnet.net>
2022-02-26 13:17:39 +0000

    subida jsons.sql

---
+++ jsons.sql
@@ -0,0 +1,11 @@
+Changed database context to 'seguretat'.
+JSON_F52E2B61-18A1-11d1-B105-00805F49916B                                                                                                                                                                                                                       
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+[{"id":1,"nom":"Administrador","actiu":true},{"id":2,"nom":"Professor","actiu":true},{"id":3,"nom":"Alumne","actiu":true}]                                                                                                                                      
+
+(1 rows affected)
+JSON_F52E2B61-18A1-11d1-B105-00805F49916B                                                                                                                                                                                                                       
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+[{"id":7,"correu":"ffernandez@politecnics.barcelona","nom":"Francisco","cognoms":"Fernández Fernández","contrasenya":"$2a$11$Y7MFjX\/wdZCMGEoBewbPwOKBJBsxNkvqgFQGMUurY2aHlZ.\/kzfES","actiu":true,"rols_id":1},{"id":8,"correu":"ivarela@salesians.sarria.cat",
+
+(1 rows affected)
